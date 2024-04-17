8782a5f4b8467c230ec2ac5150539eae307ef195
Your Namecd <danielshaw111693@gmail.com>
2024-04-16 22:26:51 -0500

    add project links and spell check

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,17 +1,20 @@
 import Col from 'react-bootstrap/Col';
 
-function ProjectCard({ title, description, imgUrl }) {
+function ProjectCard({ title, description, imgUrl, projectUrl }) {
   return (
     <Col sm={6} md={6}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+      <a href={projectUrl} target="_blank" rel="noopener noreferrer"> {/* Open link in new tab */}
+        <div className="proj-imgbx">
+          <img src={imgUrl} alt={title} />
+          <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
+          </div>
         </div>
-      </div>
+      </a>
     </Col>
   );
 }
+
 
 export default ProjectCard;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,15 +8,17 @@ function Projects() {
     {
       title: "Casual Blog",
       description:
-        "A blog website that lets you create an account, see other's posts and create your own! Using Javascript, React and CSS on the fron end, and Express, Node and MongoDB on the backend. I chose MongoDB for this database as it's more flexible with how the site will evolve over time using new features.",
+        "A blog website that lets you create an account, see other's posts and create your own! Using Javascript, React and CSS on the front end, and Express, Node and MongoDB on the backend. I chose MongoDB for this database as it's more flexible with how the site will evolve over time using new features.",
       imgUrl: "/images/Casual Blog website.png",
+      projectUrl:"https://blog-app-mo57.onrender.com"
     },
 
     {
       title: "CRUD's Book Store",
       description:
-        "A simple CRUD application where you can uploade, edit and delete book-listings! Using Javascript, React and CSS on the front end, and Express, MySQL and Node on the backend. I chose a MySQL for this database as each item has a relatively strict schema and also due to the relational nature of e-commerce websites.",
+        "A simple CRUD application where you can upload, edit and delete book-listings! Using Javascript, React and CSS on the front end, and Express, MySQL and Node on the backend. I chose a MySQL for this database as each item has a relatively strict schema and also due to the relational nature of e-commerce websites.",
       imgUrl: "/images/CRUD's Book Store.png",
+      projectUrl:"https://book-listing-app.onrender.com"
     },
   ];
   return (
